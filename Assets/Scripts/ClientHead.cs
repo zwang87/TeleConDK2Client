cca6c976d5509a536014a8781d911d97e8979101
Zhu <zw401@nyu.edu>
2015-02-28 21:57:21 +0800

    Teleconference Client
    Teleconference client with DK2, LeapMotion, Internet connection

---
+++ Assets/Scripts/ClientHead.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClientHead : MonoBehaviour {
+    public Vector3 headPosition;
+    public Vector3 headOrientation;
+
+    public GameObject oculus;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!NetworkConnection.connected) 
+        {
+            return;
+        }
+
+        if (networkView.isMine)
+        {
+            transform.position = headPosition;
+            transform.eulerAngles = headOrientation;
+        }
+        else
+        {
+            transform.position = oculus.transform.position;
+            transform.eulerAngles = oculus.transform.eulerAngles;
+            networkView.RPC("sendClientTransform", RPCMode.AllBuffered, transform.position, transform.eulerAngles);
+        }
+    }
+
+    [RPC]
+    public void sendClientTransform(Vector3 position, Vector3 eulerAngles)
+    {
+        headPosition = position;
+        headOrientation = eulerAngles;
+    }
+}
---
+++ Assets/Scripts/ObjectSync.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectSync : MonoBehaviour {
+    public Vector3 objPos;
+    public Vector3 objOrien;
+    public bool isServer;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!NetworkConnection.connected)
+        {
+            return;
+        }
+        
+      
+        
+        
+        
+
+        //if (networkView.isMine)
+        // {
+        //objPos = transform.position;
+        //objOrien = transform.eulerAngles;
+       
+        networkView.RPC("sendObjectTransform", RPCMode.All, transform.position, transform.eulerAngles);
+        
+        // }
+	}
+
+    [RPC]
+    public void sendObjectTransform(Vector3 position, Vector3 eulerAngles)
+    {
+        objPos = position;
+        objOrien = eulerAngles;
+    }
+}
